6f2515ede607704c717c53164be9736c8d13ad87
YusVel <yusupvelimetov@mail.com>
2025-02-04 21:43:27 +0300

    It works

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-extern  int  get_valid_int();
+extern  int  get_valid_int(); //первая функция на ассемблере)))
 int main()
 {
 	printf("Hello, YusVel!!!\n");
 	int a = get_valid_int();
-	printf("You entered:%d", a);
+	printf("You entered:%d\n", a);
 	return 0;
 }
